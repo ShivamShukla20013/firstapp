e35c86712e743ce875e22a1a3c29673f73bf2c5d
ShivamShukla20013 <100199959+ShivamShukla20013@users.noreply.github.com>
2022-03-17 16:10:51 +0530

    React Hello World

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+
+import React from 'react'
+import FirstComp from './components/FirstComp';
+import SecoComp from './components/SecoComp';
+import FunComp from './components/FunComp';
+import ClassComp from './components/ClassComp';
+import './components/CompCSS.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is heading.</h1>
-    </div>
+  <>
+   <div><FirstComp/></div>
+   <div><SecoComp/></div>
+  
+   
+    <BrowserRouter>
+    <Routes>
+      <Route path="/C" element={<ClassComp/>}></Route>
+      <Route path="/F" element={<FunComp/>}></Route>
+    </Routes>
+    </BrowserRouter>
+   
+  </>
   );
 }
 
