3737e9f1df56b80096fb0910514cff39495fecec
ShivamShukla20013 <100199959+ShivamShukla20013@users.noreply.github.com>
2022-03-22 11:02:59 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -2,31 +2,31 @@
 import './App.css';
 
 
-import React from 'react'
+import React from 'react';
 import FirstComp from './components/FirstComp';
 import SecoComp from './components/SecoComp';
 import FunComp from './components/FunComp';
 import ClassComp from './components/ClassComp';
 import './components/CompCSS.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 
 function App() {
   return (
-  <>
-   <div><FirstComp/></div>
-   <div><SecoComp/></div>
-  
-   
+    <>
+    <div>
+      <h1>This is a div.</h1>
+    </div>
+    <FirstComp/>
+    <SecoComp/>
+
     <BrowserRouter>
     <Routes>
-      <Route path="/C" element={<ClassComp/>}></Route>
-      <Route path="/F" element={<FunComp/>}></Route>
+      <Route path='/c' element={<ClassComp/>}></Route>
+      <Route path='/f' element={<FunComp/>}></Route>
     </Routes>
     </BrowserRouter>
-   
-  </>
+    </>
   );
 }
 
